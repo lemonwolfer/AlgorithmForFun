a25fea7ec5738de067df59ea2d724772d038af8a
lichao@cnhnkj.com <lichao@cnhnkj.com>
2019-04-22 17:32:08 +0800

    rb

--- src/main/java/bsts/RBBST.java
+++ src/main/java/bsts/RBBST.java
@@ -1,10 +1,35 @@
 package bsts;
 
+import lombok.Data;
+
 public class RBBST<Key extends Comparable<Key>,Value> {
     private static final boolean RED = true;
     private static final boolean BLACK = false;
-    private Node root;
-    public
+    private Node root ;
+
+    public RBBST() {
+        this.root = null;
+    }
+
+    public Node put(Key key,Value value){
+        Node root = doPut(this.root, key, value);
+        return root;
+    }
+
+    private Node doPut(Node entryNode, Key key, Value value) {
+        Node newNode = new Node(key,value,0,RED);
+        if (entryNode == null){
+            newNode.setColor(BLACK);
+            return newNode;
+        }
+        if(key.compareTo(entryNode.key)<0)
+            entryNode.left = doPut(entryNode.left,key,value);
+        else if(key.compareTo(entryNode.key)>0)
+            entryNode.right = doPut(entryNode.right,key,value);
+        else
+            entryNode.value = value;
+    }
+    @Data
     private class Node{
         private Key key;
         private Value value;
@@ -16,6 +41,10 @@ public class RBBST<Key extends Comparable<Key>,Value> {
             this.key = key;
             this.value = value;
             N = n;
+        }
+        private Node(Key key, Value value, int n,boolean color) {
+            this(key,value,n);
+            this.color = color;
         }
     }
     private boolean isRed(Node x){
